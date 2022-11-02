26a238adc5f4c998b1a4457413b2321bcd5f8c37
Maxime Vauclin <maxime.vauclin@epita.fr>
2022-11-02 21:04:39 +0100

    save au cas ou

--- expense.py
+++ expense.py
@@ -1,30 +1,69 @@
 from PyInquirer import prompt
+import csv
+from user import read_users
+
 
 expense_questions = [
     {
-        "type":"input",
-        "name":"amount",
-        "message":"New Expense - Amount: ",
+        "type": "input",
+        "name": "amount",
+        "message": "New Expense - Amount: ",
     },
     {
-        "type":"input",
-        "name":"label",
-        "message":"New Expense - Label: ",
+        "type": "input",
+        "name": "label",
+        "message": "New Expense - Label: ",
     },
     {
-        "type":"input",
-        "name":"spender",
-        "message":"New Expense - Spender: ",
+        "type": "input",
+        "name": "spender",
+        "message": "New Expense - Spender: ",
     },
 
 ]
 
 
+def add_stop_option():
+    L = read_users()
+    L.append("Stop")
+    return L
+
+
+users = [
+    {
+        'type': 'rawlist',
+        'name': 'users',
+        'message': 'Select users or select Stop when you are done : ',
+        'choices': add_stop_option(),
+    },
+]
+
+
+def export_expense(infos):
+    with open('expense_report.csv', 'a') as f:  # You will need 'wb' mode in Python 2.x
+        w = csv.DictWriter(f, infos.keys())
+        w.writeheader()
+        w.writerow(infos)
+
 
 def new_expense(*args):
     infos = prompt(expense_questions)
+
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
     print("Expense Added !")
+    tmp = ""
+    List_of_users_involved = []
+    while tmp != "Stop":
+        user = prompt(users)
+        tmp = user["users"]
+        if tmp == "Stop":
+            break
+        List_of_users_involved.append(tmp)
+
+    cost_of_each = int(infos["amount"]) // (len(List_of_users_involved) + 1)
+    print("Each people need to refund : " +
+          str(cost_of_each) + " to " + infos["spender"])
+
+    infos["people_involved"] = List_of_users_involved
+    export_expense(infos)
     return True
-
-
--- user.py
+++ user.py
@@ -1,8 +1,37 @@
 from PyInquirer import prompt
-user_questions = [
+import csv
 
+user_questions = [
+    {
+        "type": "input",
+        "name": "Name",
+        "message": "Name: ",
+    },
 ]
 
+
+def export_user(infos):
+    with open('user_report.csv', 'a') as f:  # You will need 'wb' mode in Python 2.x
+        w = csv.writer(f)
+        for key, value in infos.items():
+            w.writerow([key, value])
+        f.close()
+
+
+def read_users():
+    with open('user_report.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        L = []
+        for row in reader:
+            L.append(row[1])
+        csv_file.close()
+    return L
+
+
 def add_user():
+    infos = prompt(user_questions)
+    export_user(infos)
+
+    print("User Added !")
     # This function should create a new user, asking for its name
     return
